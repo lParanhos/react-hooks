4ad2b8dc8295bcb4b5cc7ce4726ab2e81f8951af
Leandro Paranhos <leandro-paranhos@hotmail.com>
2019-12-27 13:06:12 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
 
@@ -7,12 +7,44 @@ function App() {
     setNewTech('');
   }
 
-  const [techs, setTech] = useState([
-    'ReactJS',
-    'React Native'
-  ]);
+  /** Fazemos uma desestruturação do useState
+   * pois o mesmo nos retorna no primeiro parametro o nosso estado,
+   *  e no segundo a função responsável por alterar o nosso estado
+   */
+  const [techs, setTech] = useState([]);
 
   const [newTech, setNewTech] = useState('')
+
+  /** O useEffect veio para substituir os ciclos de vida de um componente baseado em classe, 
+   * por exemplo: componentDidMount, componentDidUpdate e componentWillUnMount.
+   * O primeiro parametro é a função que vai ser executada e o segundo é o "quando" ela vai ser eexecutada
+   * no caso, abaixo, sempre que meu estado de techs forem alterados, será dispara a função. 
+   * Os effects sempre são executados em primeira instancia
+   * */
+
+  /**Component didMount 
+     * Como não passamos nenhum "estado" para ser monitorado, o nosso effect é executado apenas uma vez
+    */
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('tech');
+    console.log(storageTechs)
+    if (storageTechs) {
+      setTech(JSON.parse(storageTechs))
+    }
+
+    /** Para conseguir ter o mesmo efeito do componentWillUnmount, basta retornar uma função 
+     * Assim, antes de o componente ser desmontado, consiguimos fazer algo
+     */
+    //return () => {};
+  }, [])
+
+
+  /** Faz a mesma coisa coisa que o component didUpdate */
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(techs))
+  }, [techs]);
+
+
 
   return (
     <>
